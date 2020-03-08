484eb8e862566e1dff2ce226adcd7712a961e5a4
Shane Wilton <shane.wilton@gmail.com>
2020-03-08 12:02:20 -0700

    Clean up the code a little bit

--- lib/tiferet.ex
+++ lib/tiferet.ex
@@ -2,10 +2,8 @@ defmodule Tiferet do
   defmacro defpmodule(name, dependencies, do: body) do
     quote do
       defmodule unquote(name) do
-        @dependencies unquote(dependencies)
-
         def __dependencies__ do
-          @dependencies
+          unquote(dependencies)
         end
 
         defmacro __using__(_opts) do
@@ -18,28 +16,38 @@ defmodule Tiferet do
   end
 
   defmacro definst(pmodule, name, dependencies) do
+    quote do
+      validate_dependencies(unquote(dependencies), unquote(pmodule))
+
+      defmodule unquote(name) do
+        use unquote(pmodule)
+
+        inject_dependencies(unquote(dependencies), unquote(pmodule))
+      end
+    end
+  end
+
+  def validate_dependencies(dependencies, pmodule) do
+    for {k, v} <- dependencies do
+      case Keyword.fetch!(pmodule.__dependencies__, k) do
+        n when is_function(v, n) ->
+          :ok
+        mod when is_atom(mod) and is_atom(v) ->
+          :ok
+      end
+    end
+  end
+
+  defmacro inject_dependencies(dependencies, pmodule) do
     dependency_reference =
       quote unquote: false do
         unquote(dependency)
       end
 
     quote do
-      for {k, v} <- unquote(dependencies) do
-        case Keyword.fetch!(unquote(pmodule).__dependencies__, k) do
-          n when is_function(v, n) ->
-            :ok
-          mod when is_atom(mod) and is_atom(v) ->
-            :ok
-        end
-      end
-
-      defmodule unquote(name) do
-        use unquote(pmodule)
-
-        for {dependency, _value} <- unquote(pmodule).__dependencies__ do
-          def unquote(dependency_reference)() do
-            Keyword.fetch!(unquote(dependencies), unquote(dependency_reference))
-          end
+      for {dependency, _value} <- unquote(pmodule).__dependencies__ do
+        def unquote(dependency_reference)() do
+          Keyword.fetch!(unquote(dependencies), unquote(dependency_reference))
         end
       end
     end

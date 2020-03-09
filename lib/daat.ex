8a9c3533405b9a28cd202c48e8daf5325e2d83f2
Shane Wilton <shane.wilton@gmail.com>
2020-03-08 23:48:33 -0700

    Move dependency validation / injection to it's own module

--- lib/daat.ex
+++ lib/daat.ex
@@ -1,6 +1,4 @@
 defmodule Daat do
-  alias Daat.InvalidDependencyError
-
   defmacro defpmodule(name, dependencies, do: body) do
     quote do
       defmodule unquote(name) do
@@ -17,7 +15,9 @@ defmodule Daat do
 
   defmacro definst(pmodule, instance, dependencies) do
     quote do
-      validate_dependencies(
+      require Daat.Dependency
+
+      Daat.Dependency.validate(
         unquote(dependencies),
         unquote(pmodule),
         unquote(instance)
@@ -26,41 +26,7 @@ defmodule Daat do
       defmodule unquote(instance) do
         use unquote(pmodule)
 
-        inject_dependencies(unquote(dependencies), unquote(pmodule))
-      end
-    end
-  end
-
-  def validate_dependencies(dependencies, pmodule, instance) do
-    for {dep_name, dep_value} <- dependencies do
-      declaration = Keyword.fetch!(pmodule.__dependencies__, dep_name)
-
-      valid =
-        cond do
-          is_integer(declaration) ->
-            is_function(dep_value, declaration)
-
-          is_atom(declaration) ->
-            is_atom(dep_value)
-        end
-
-      if not valid do
-        raise InvalidDependencyError.new(pmodule, instance, dep_name)
-      end
-    end
-  end
-
-  defmacro inject_dependencies(dependencies, pmodule) do
-    dependency_reference =
-      quote unquote: false do
-        unquote(dependency)
-      end
-
-    quote do
-      for {dependency, _value} <- unquote(pmodule).__dependencies__() do
-        def unquote(dependency_reference)() do
-          Keyword.fetch!(unquote(dependencies), unquote(dependency_reference))
-        end
+        Daat.Dependency.inject(unquote(dependencies), unquote(pmodule))
       end
     end
   end
---
+++ lib/daat/dependency.ex
@@ -0,0 +1,37 @@
+defmodule Daat.Dependency do
+  alias Daat.InvalidDependencyError
+
+  def validate(dependencies, pmodule, instance) do
+    for {dep_name, dep_value} <- dependencies do
+      declaration = Keyword.fetch!(pmodule.__dependencies__, dep_name)
+
+      valid =
+        cond do
+          is_integer(declaration) ->
+            is_function(dep_value, declaration)
+
+          is_atom(declaration) ->
+            is_atom(dep_value)
+        end
+
+      if not valid do
+        raise InvalidDependencyError.new(pmodule, instance, dep_name)
+      end
+    end
+  end
+
+  defmacro inject(dependencies, pmodule) do
+    dependency_reference =
+      quote unquote: false do
+        unquote(dependency)
+      end
+
+    quote do
+      for {dependency, _value} <- unquote(pmodule).__dependencies__() do
+        def unquote(dependency_reference)() do
+          Keyword.fetch!(unquote(dependencies), unquote(dependency_reference))
+        end
+      end
+    end
+  end
+end
